087d40317efa0631471a330193c31909f563e33d
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-05 16:27:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,37 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(title)
+    song = self.new
+    song.name = title
+    song
+  end
+
+  def self.create_by_name(title)
+    song = self.create
+    song.name = title
+    song
+  end
+
+  def self.find_by_name(title)
+    search_result = self.all.find {|p| p.name == title}
+    search_result
+  end
+
+  def self.find_or_create_by_name(title)
+    search_result = self.find_by_name(title)
+      if search_result == true
+        return search_result
+      else
+        self.create_by_name(title)
+      end
+
+    end
+
 end
