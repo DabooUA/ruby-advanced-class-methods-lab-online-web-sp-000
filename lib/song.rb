6f78ea47f371f87748907383a87bf9358201f7ba
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-05 16:39:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,9 +42,18 @@ class Song
       end
     end
 
-    def self.alphabetical
+  def self.alphabetical
     sort_result = self.all.sort_by {|song| song.name}
     sort_result
-    end
+  end
+
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
 
 end
