81d1ef60bc349e4590f2e588bb31839c4f3971f3
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-05 16:29:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
   def self.find_or_create_by_name(title)
     search_result = self.find_by_name(title)
       if search_result then
-      search_result
+      return search_result
       else
         self.create_by_name(title)
       end
